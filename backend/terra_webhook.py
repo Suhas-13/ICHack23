c73e8389b20c17a0d10a4cf9e9a804b78ff9236f
Suhas Hariharan <hariharan774531@sas.edu.sg>
2023-02-05 00:24:29 +0000

    added terra webhook process

---
+++ backend/terra_webhook.py
@@ -0,0 +1,113 @@
+import os
+import json
+import asyncio
+import websockets
+import aiohttp
+from datetime import datetime
+from flask import Flask, jsonify, request, Request
+
+app = Flask(__name__)
+app.debug = True
+
+hr_list = []
+time_list = []
+phone_uid_to_accounts = {}
+sessions = {}
+accounts = {}
+
+WS_CONNECTION = "wss://ws.tryterra.co/connect"
+
+async def generate_token():
+    async with aiohttp.ClientSession() as session:
+        headers = {
+            "Accept": "application/json",
+            "dev-id": "test-screening-dev-hZNKTHrzFJ",
+            "x-api-key": "3cb3a8b7c0321ef5e54a67ae64660eacd5bd1018fee36997921ed63d5109793f"
+        }
+
+        async with session.post("https://ws.tryterra.co/auth/developer", headers=headers) as resp:
+            token = await resp.json()
+            return token["token"]
+
+async def init_ws():
+    while True:
+        token = await generate_token()
+        try:
+            async with websockets.connect(WS_CONNECTION) as socket:
+                expecting_heart_beat_ack = False
+
+                async def heart_beat_after(n):
+                    await asyncio.sleep(n)
+                    await heart_beat()
+                async def heart_beat():
+                    print("HEART BEAT")
+                    nonlocal expecting_heart_beat_ack
+                    heart_beat_payload = json.dumps({'op': 0})
+                    asyncio.create_task(socket.send(heart_beat_payload))
+                    expecting_heart_beat_ack = True
+                
+
+                async for message in socket:
+                    message = json.loads(message)
+                    if message["op"] == 2:
+                        asyncio.create_task(heart_beat_after(message["d"]["heartbeat_interval"] / 1000))
+                        payload = json.dumps(generate_payload(token))
+                        await socket.send(payload)
+                        print(payload)
+                    if message["op"] == 5:
+                        if message['d']['val'] <= 0.1:
+                            continue
+                        print(message)
+                        uid = message["uid"]
+                        if uid not in phone_uid_to_accounts:
+                            continue
+                        else:
+                            account = phone_uid_to_accounts[uid]
+                            active_session = accounts[account]['active_session']
+                            if active_session is not None and active_session in sessions:
+                                if "hr" not in sessions[active_session]:
+                                    sessions[active_session]['hr'] = {}
+                                epoch_time = int(datetime.strptime(message['d']['ts'], "%Y-%m-%dT%H:%M:%S.%fZ").timestamp())
+                                sessions[active_session]['hr'][epoch_time] = message['d']['val']
+
+        except Exception as e:
+            print(e)
+
+def generate_payload(token):
+    return {
+        "op": 3,
+        "d": {
+            "token": token,
+            "type": 1
+        }
+    }
+
+@app.route("/api/hr")
+def get_hr_data():
+    return jsonify(hr_list)
+
+@app.route("/process_image")
+def process_image():
+    request_data = request.get_json()
+    image_data = request_data['image_data']
+    epoch_time = request_data['epoch_time']
+    session_id = request_data['session_id']
+    account_id = request_data['account_id']
+    if account_id not in accounts:
+        accounts[account_id] = {"active_session": None}
+    if session_id not in sessions:
+        sessions[session_id] = {}
+        accounts[account_id]['active_session'] = session_id
+    if "images" not in sessions[session_id]:
+        sessions[session_id]['images'] = {}
+    sessions[session_id]['images'][epoch_time] = {"image_data": image_data, "video_time": request_data["video_time"]}
+    return jsonify({"success": True})
+
+@app.route("/")
+def root():
+    return app.send_static_file("index.html")
+
+if __name__ == "__main__":
+    asyncio.run(init_ws())
+    port = int(os.environ.get("PORT", 3000))
+    app.run(port=port)
